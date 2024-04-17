5468134afbc47f430420265a08ac20a26e27bf59
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-04-17 14:33:55 -0300

    fix: componentizando button whatsapp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ import { CarrouselJobs } from "@/components/carrousel-jobs";
 import { Footer } from "@/components/footer";
 import { motion, useAnimation, useInView } from "framer-motion";
 import { WhatsappLogo } from "@phosphor-icons/react";
+import { ButtonWhatsAppFixed } from "@/components/button-whatsapp-fixed";
 
 
 export default function Home() {
@@ -146,19 +147,6 @@ export default function Home() {
             <div>
               <p className="text-cinzaCami text-lg sm:text-2xl">Método Cami</p>
             </div>
-
-            {/* <div className="flex flex-col gap-3 items-center">
-            <div className="flex flex-col sm:text-2xl text-lg text-pretoCami items-center mb-2 mx-1 max-w-full sm:max-w-[600px]">
-              <h3 className="w-full text-justify sm:text-center">Utilizamos uma metodologia de 7 passos
-                clara e objetiva, onde o cliente participa de todo o processo
-              </h3>
-            </div>
-            <div className="mb-2">
-              <p className="text-cinzaCami text-lg w-full text-center">A criação de marca é uma via de mão dupla</p>
-            </div>
-            <div>
-              <p className="text-cinzaCami text-lg sm:text-2xl">Método Cami</p>
-            </div> */}
             <motion.div
               initial={{ opacity: 0, y: -30, scale: 0.7 }}
               whileInView={{ opacity: 1, y: 0, scale: 1 }}
@@ -249,17 +237,7 @@ export default function Home() {
         </div>
         <CarrouselJobs />
       </section>
-      <div className="flex fixed bottom-6 right-3 z-50 rounded-full shadow-xl">
-        <button
-          onClick={() =>
-            sendMensage(
-              554199769766,
-              "Olá! Vim através do site da Cami Agência Criativa."
-            )
-          }
-          className="cursor-pointer p-3 bg-green-500 hover:bg-green-600 transition-all rounded-full shadow-md hover:shadow-2xl"><WhatsappLogo
-            className="w-12 h-12 text-zinc-50 " /></button>
-      </div>
+      <ButtonWhatsAppFixed />
       <Footer />
     </div>
   );

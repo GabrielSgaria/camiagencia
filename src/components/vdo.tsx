c336187acb4e790b379839ece504d539c41ac8bb
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-05-03 17:14:30 -0300

    feat: video1

--- src/components/vdo.tsx
+++ src/components/vdo.tsx
@@ -1,4 +1,5 @@
-import React, { useRef, useEffect } from 'react';
+import { useInView } from 'framer-motion';
+import React, { useRef, useEffect, useState } from 'react';
 
 interface VideoPlayerProps {
     src: string;
@@ -6,12 +7,13 @@ interface VideoPlayerProps {
 
 export function VideoPlayer({ src }: VideoPlayerProps) {
     const videoRef = useRef<HTMLVideoElement | null>(null);
+    const isInView = useInView(videoRef, { once: true });
 
     useEffect(() => {
-        if (videoRef.current) {
-            videoRef.current.play();
+        if (isInView) {
+            videoRef.current?.play();
         }
-    }, []);
+    }, [isInView]);
 
     return (
         <video ref={videoRef} controls preload="auto" className="w-full h-full">

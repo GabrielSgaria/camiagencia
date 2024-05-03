9fdc5b0f3ee306ea5bd9fac6ef37f71c2a9a26a8
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-05-03 16:48:17 -0300

    feat: play video

--- src/components/vdo.tsx
+++ src/components/vdo.tsx
@@ -8,36 +8,13 @@ export function VideoPlayer({ src }: VideoPlayerProps) {
     const videoRef = useRef<HTMLVideoElement | null>(null);
 
     useEffect(() => {
-        const handleScroll = () => {
-            if (videoRef.current) {
-                const videoRect = videoRef.current.getBoundingClientRect();
-                const windowHeight = window.innerHeight || document.documentElement.clientHeight;
-                const isVideoVisible = videoRect.top < windowHeight && videoRect.bottom >= 0;
-                
-                if (isVideoVisible && videoRef.current.paused) {
-                    videoRef.current.play();
-                } else if (!isVideoVisible && !videoRef.current.paused) {
-                    videoRef.current.pause();
-                }
-            }
-        };
-
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
+        if (videoRef.current) {
+            videoRef.current.play();
+        }
     }, []);
 
     return (
-        <video
-            ref={videoRef}
-            controls
-            preload="auto"
-            className="w-full h-full"
-            onClick={() => {
-                if (videoRef.current) {
-                    videoRef.current.play();
-                }
-            }}
-        >
+        <video ref={videoRef} controls preload="auto" className="w-full h-full">
             <source src={src} type="video/mp4" />
         </video>
     );

2f1714543f92e5a92d05e6dec46a482be6e34ef2
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-05-06 14:58:41 -0300

    feat: novo video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -144,9 +144,9 @@ export default function Home() {
         </div>
       </section>
       <section className="flex items-center justify-center h-[250px] sm:h-[400px] lg:h-[594px] container mx-auto mt-10 relative px-0" id="about">
-        <VideoPlayer src="https://media.graphassets.com/ChxhkMSvSfObFUyTkwHK" />
-        <Image width={900} height={900} alt="Icon 1" src='/image/1.png' className="absolute left-0 -top-4 w-36 sm:top-0 md:w-36 lg:left-10 lg:w-60 md:top-1/3 z-50" />
-        <Image width={900} height={900} alt="Icon 1" src='/image/2.png' className="absolute right-0 w-36 -bottom-14 sm:top-0 md:w-36 lg:right-10 lg:top-[50%] md:top-1/3 lg:w-60 z-50" />
+        <VideoPlayer src="https://media.graphassets.com/ahEpFiS26sBSuTYC98fg" />
+        {/* <Image width={900} height={900} alt="Icon 1" src='/image/1.png' className="absolute left-0 -top-4 w-36 sm:top-0 md:w-36 lg:left-10 lg:w-60 md:top-1/3 z-50" />
+        <Image width={900} height={900} alt="Icon 1" src='/image/2.png' className="absolute right-0 w-36 -bottom-14 sm:top-0 md:w-36 lg:right-10 lg:top-[50%] md:top-1/3 lg:w-60 z-50" /> */}
       </section>
 
       <section className="pt-16" id="services">

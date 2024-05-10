7915a7b88b6a92015446e243ca54c98e4bce4ded
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-05-10 15:48:58 -0300

    feat:autoplay

--- src/components/video-desktop.tsx
+++ src/components/video-desktop.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function VideoDesktop() {
     return (
         <div className="flex items-center justify-center h-[250px] sm:h-[400px] lg:h-[594px] container mx-auto mt-10 relative px-0" id="about">
-            <iframe className="w-full h-full relative z-0" src="https://www.youtube.com/embed/_zu1wB6aO2E?si=TTZBg36LhFFnj_yB&amp;controls=0"
+            <iframe className="w-full h-full relative z-0" src="https://www.youtube.com/embed/_zu1wB6aO2E?si=TTZBg36LhFFnj_yB&amp;controls=0&autoplay=1"
                 title="YouTube video player"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; allowfullscreen"
             >
